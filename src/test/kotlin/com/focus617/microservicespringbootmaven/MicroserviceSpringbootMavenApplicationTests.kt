a1ad26c4f8e57c06b262f36bcff42d5dff1a57ff
focus617 <13980668829@163.com>
2022-09-30 10:09:17 +0800

    Baseline.

---
+++ src/test/kotlin/com/focus617/microservicespringbootmaven/MicroserviceSpringbootMavenApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.focus617.microservicespringbootmaven
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroserviceSpringbootMavenApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
